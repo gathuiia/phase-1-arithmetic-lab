e4e642ad68f06fbda8571087bf85fdb765e62db8
Ian Gathui <ian.gathui@student.moringaschool.com>
2022-11-25 16:23:58 +0300

    lab completed

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+const num1 = 2;
+const num2 = 31;
+let multiply;
+multiply = num1 * num2;
+
+let random;
+random = Math.ceil(Math.random() * 1000);
+
+const num3 = 10;
+const num4 = 6;
+let mod;
+mod = num3 % num4;
+
+
+let max; 
+max = Math.max(4, 8, 12, 16, 20);
